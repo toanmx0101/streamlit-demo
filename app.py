7feed19358e78bc2452cf38cc5d3d8eb1b3ac83d
Toan Mai <wth8647@gmail.com>
2023-05-12 21:53:56 +0700

    Init commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit  as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title='Survey Results')
+st.header('Survey results 2021')
+st.subheader('Survey results 2021')
